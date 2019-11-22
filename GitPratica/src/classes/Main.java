b20330b79eb87e0dc186722108dfd0c568e1cf2c
Raylson Carlos <raylsoncs@gmail.com>
2019-11-21 23:01:33 -0300

    Commit in master to brach merge afterwards!

--- GitPratica/src/classes/Main.java
+++ GitPratica/src/classes/Main.java
@@ -2,6 +2,7 @@ package classes;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, from the otherside!");		
+		System.out.println("Hello, from the otherside!");	
+		System.out.println("modificação!");
 	}
 }
